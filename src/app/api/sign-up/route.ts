600d308bcffbe25e30c9cbbe809bf3b446ebf199
gativarshney <gativarshney2006@gmail.com>
2025-09-17 20:58:16 +0530

    feat(auth): complete user signup route with email verification

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -25,7 +25,19 @@ export async function POST(request : Request){
         const verifyCode = Math.floor(100000 + Math.random() * 900000).toString()
 
         if(existingUserByEmail){
-            true // TODO: Back here
+            if(existingUserByEmail.isVerified){
+                return Response.json({
+                    success: false,
+                    message: "User already exist with this email"
+                }, {status: 400})
+            }
+            else{
+                const hashedPassword = await bcrypt.hash(password, 10);
+                existingUserByEmail.password = hashedPassword;
+                existingUserByEmail.verifyCode = verifyCode;
+                existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000)
+                await existingUserByEmail.save()
+            }
         }
         else{
             const hashedPassword = await bcrypt.hash(password, 10)
@@ -44,7 +56,23 @@ export async function POST(request : Request){
             })
             await newUser.save()
         }
-        // send verification email
+
+        //* send verification email
+        const emailResponse = await sendVerificationEmail(
+            email,
+            username,
+            verifyCode
+        )
+        if(!emailResponse.success){
+            return Response.json({
+                success: false,
+                message: emailResponse.message
+            }, {status: 500})
+        }
+        return Response.json({
+            success: true,
+            message: "User registered successsfully. Please verify your email"
+        }, {status: 201})
 
     } catch (error) {
         console.log("Error Registering User", error);
